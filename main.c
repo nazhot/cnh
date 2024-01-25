18374cecd959ac750534bfec378051d3b026725a
nazhot <15216154+nazhot@users.noreply.github.com>
2024-01-25 13:15:39 -0700

    Added .gitignore, Got Files Created

--- main.c
+++ main.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 
 
-int main( int argc, char **argv ) {
+int main( int argc, char *argv[] ) {
+
+    if ( argc == 1 ) {
+        printf( "No arguments provided, doing nothing\n" );
+        return 0;
+    }
+
+    for ( int i = 1; i < argc; ++i ) {
+        char *fileName = argv[i];
+        printf( "%s\n", fileName );
+        char headerFileName[256];
+        char sourceFileName[256];
+        unsigned int charPosition = 0;
+        while ( *fileName ) {
+            headerFileName[charPosition] = *fileName;
+            sourceFileName[charPosition++] = *fileName;
+            fileName++;
+        }
+        headerFileName[charPosition] = '.';
+        sourceFileName[charPosition++] = '.';
+        headerFileName[charPosition] = 'h';
+        sourceFileName[charPosition++] = 'c';
+        
+        FILE *headerFile = fopen( headerFileName, "w" );
+        FILE *sourceFile = fopen( sourceFileName, "w" );
+    }
+
     return 0;
 }
